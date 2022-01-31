4925fb03005ffed02c8197ec9e73f41901907814
Abhishek <94548108+ignabhishek@users.noreply.github.com>
2022-01-31 23:17:52 +0530

    Update Prog11.py

--- CO2/Prog11.py
+++ CO2/Prog11.py
@@ -5,4 +5,4 @@ rectangle = lambda a, b: a * b
 print("Area of the rectangle is", rectangle(10, 5))
 
 triangle = lambda b, h: (b * h) / 2
-print("Area of the rectangle is", triangle(4, 2))
+print("Area of the triangle is", triangle(4, 2))
