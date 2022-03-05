db3bcb3a5455a542a360a3fab44a61830d9852e4
ignabhishek <itsabhishekchandra@gmail.com>
2022-03-05 19:56:32 +0530

    co1 programs

---
+++ CO1/program2.py
@@ -0,0 +1,7 @@
+# Display future leap years from current year to a final year entered by user.
+
+current = int(input("enter current year:"))
+last = int(input("enter final year:"))
+for i in range(current, last + 1):
+    if i % 4 == 0 and i % 100 != 0 or i % 400 == 0:
+        print(i)
